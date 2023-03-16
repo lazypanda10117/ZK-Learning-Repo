aa35da61e1b3f8e66aa2e8e59ebbb742c7a14bbc
Jeffrey Kam <jeffreykam0415@gmail.com>
2023-03-16 19:58:00 +0800

    Adding zk proof for discrete log

---
+++ Lecture 1/zk_discrete_log.py
@@ -0,0 +1,48 @@
+import random
+import hashlib
+
+from typing import Tuple
+
+
+LARGE_PRIME = 273389558745553615023177755634264971227
+GENERATOR = 29155293994990157899330610805491402811 # random.randint(1, LARGE_PRIME)
+
+def hash_helper(g, y, t, p) -> int:
+    c_sha = hashlib.sha3_256((str(g) + str(y) + str(t)).encode()) # verifier random challenge
+    c = int(c_sha.hexdigest(), 16) % p # modulo to reduce size
+    return c
+
+# This is the prover (non-interactive)
+# x = secret
+# g = generator
+# p = large prime for the field
+# Returns: residual (y) and proof of knowledge (pf)
+def discreteLogProof(x, g ,p) -> Tuple[int, int]:
+    y = pow(g, x, p) # public
+    r = random.randint(0, p) # prover random bit
+    t = pow(g, r, p) # prover to verifier 
+    c = hash_helper(g, y, t, p) # modulo to reduce size
+    s = (c * x + r) % (p - 1) # the proof
+    pf = (s, t)
+    # print("Y", y)
+    # print("R", r)
+    # print("T", t)
+    # print("C", c)
+    # print("S", s)
+    # print("PF", pf)
+    return (y, pf)
+
+# This is the verifier
+def verifiyProof(y, g, p, pf) -> bool:
+    s, t = pf
+    c = hash_helper(g, y, t, p)
+    return pow(g, s, p) == ((t * pow(y, c, p)) % p)
+
+def main():
+    x = 150
+    y, pf = discreteLogProof(x, GENERATOR, LARGE_PRIME)
+    result = verifiyProof(y, GENERATOR, LARGE_PRIME, pf)
+    print("Discrete Log Proof:", y, pf)
+    print("Verify Proof:", result)
+
+main()
